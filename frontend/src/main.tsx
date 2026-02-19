c8ba0c0a15f55c00030f722185659fe661f5bc82
Radman <rdmnr@protonmail.com>
2026-02-18 16:15:24 -0800

    fix(frontend): use valid PostHog person_profiles type value

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,7 +7,7 @@ import App from './App.tsx'
 posthog.init('phc_R7ZAuPFBNbxkup1BuUERgB7zRw1NC4htBd4Eu9N49MQ', {
   api_host: 'https://radman.dev/ingest',
   ui_host: 'https://us.posthog.com',
-  person_profiles: 'anonymous',
+  person_profiles: 'identified_only',
   autocapture: true,
 })
 
